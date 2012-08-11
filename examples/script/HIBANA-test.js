edd4a0f65ed3698fc5303a55407e9b6be2aac7aa
Michael Barger <MikeBarger@gmail.com>
2012-08-11 11:11:09 -0700

    changed directory structure to be similar to three.js; added license; deleted unnecessary files

---
+++ examples/script/HIBANA-test.js
@@ -0,0 +1,204 @@
+
+
+
+// global constants
+var WIDTH, HEIGHT, VIEW_ANGLE = 65, ASPECT, NEAR = 5, FAR = 100;
+var ROOM_DIM = 50, OBJECT_SIZE = 2.5, OBJECT_DETAIL = 30;
+var MAX_CAMERA_ANGLE = Math.PI / 5;
+var CAMERA_RADIUS = ROOM_DIM / 2 - OBJECT_SIZE;
+var CAMERA_HOME = new THREE.Vector3( 0, 0, ROOM_DIM / 2 - OBJECT_SIZE );
+var CAMERA_TARGET = new THREE.Vector3( 0, 0, 0 );
+var AZIMUTH_RANGE = OBJECT_SIZE; 
+var MOUSE_SPEED = 0.0001
+
+// global variables
+var azimuth = 0, zenith = 0, mouse_x = 0, mouse_y = 0, mouse_decay = true, mouse_is_down = false;
+var renderer, composer, camera, scene;
+var mouse_decay;
+var objects;
+var hibana;
+
+
+// ****** Executes as soon as the window has loaded
+$(window).load( function() {
+	// initialize the 3D engine
+	init3D();
+	
+	$("#main3d").mousedown( function() {
+		mouse_decay = false;
+		mouse_is_down = true;
+		mouse_x = mouse_y = 0;
+		$("#main3d").bind( "mousemove", function( event ) {
+			mouse_is_down = true;
+			mouse_x = event.pageX - $("#main3d").position().left - $("#main3d").width() / 2;
+			mouse_y = event.pageY - $("#main3d").position().top - $("#main3d").height() / 2;
+		});
+	});
+	
+	$("body").mouseup( function() {
+		mouse_decay = true;
+		mouse_is_down = false;
+		$("#main3d").unbind( "mousemove" );
+	});
+	
+	$("#main3d").mouseleave( function() {
+		mouse_decay = true;
+		mouse_is_down = false;
+		$("#main3d").unbind( "mousemove" );
+	});
+	
+});
+
+// ****** Makes sure the 3D draws properly even if the browser window is resized
+$(window).resize( function() {
+
+	WIDTH = $("#main3d").width(); HEIGHT = $("#main3d").height();
+	camera.aspect = WIDTH / HEIGHT;
+	camera.updateProjectionMatrix();
+	renderer.setSize( WIDTH, HEIGHT );
+	
+});
+
+
+
+
+// ****** Initializes the 3D environment
+function init3D() {
+
+	// initialize 3d globals
+	WIDTH = $("#main3d").width(); HEIGHT = $("#main3d").height(); ASPECT = WIDTH / HEIGHT;
+	
+	// initialize renderer
+	renderer = new THREE.WebGLRenderer( { antialias : true, shadowMapEnabled : true, shadowMapSoft : true, gammaInput : true, gammaOutput : true } );
+    renderer.setSize( WIDTH, HEIGHT );
+	renderer.setClearColorHex( 0xFFFFFF, 1 );
+    $("#main3d").append( renderer.domElement );
+
+    scene = new THREE.Scene();
+
+	createRoom();
+	createCamera();	
+	
+	hibana = new HIBANA( scene, new THREE.Vector3( -100, -100, -100 ) );
+	
+	createObjects( 25 );
+	createEmitters();
+	createLights();
+	
+	animate();
+	
+}
+
+function createRoom() {
+	var geo = new THREE.CubeGeometry( ROOM_DIM,  ROOM_DIM,  ROOM_DIM, 10, 10, 10 );
+	var materials = [	new THREE.MeshLambertMaterial( { color : 0xBBBBFF, shading : THREE.FlatShading } ),
+				new THREE.MeshBasicMaterial( { color : 0x444444, shading : THREE.FlatShading, wireframe : true, wireframeLinewidth : 4, opacity : 0.3, transparent : true } ) ];
+			
+	var room_mesh = THREE.SceneUtils.createMultiMaterialObject( geo, materials );
+	room_mesh.position.set( 0, 0, 0 );
+	room_mesh.children[0].doubleSided = true;
+	room_mesh.children[1].doubleSided = true;
+	scene.add( room_mesh );
+}
+
+function createCamera() {
+    camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR );
+	camera.position = new THREE.Vector3().copy( CAMERA_HOME );
+	camera.lookAt( CAMERA_TARGET );
+    scene.add( camera );
+}
+
+function createObjects( objectCount ) {
+	objects = [];
+	for ( var i = 0; i < objectCount; i++ ) {
+		var object = new THREE.Mesh( new THREE.SphereGeometry( OBJECT_SIZE, OBJECT_DETAIL, OBJECT_DETAIL ),
+				new THREE.MeshPhongMaterial( { color: 0xFF0000, metal: true, opacity: 1.0 } ) );
+		object.position = createRandomPositionWithinRoom();
+		scene.add( object );
+		objects.push( object );
+	}
+}
+
+function createEmitters() {
+	for ( o in objects ) {
+		hibana.addEmitter( { mesh: objects[o], particle_color: new THREE.Color( 0xff9100 ) } )
+	}
+	hibana.play();
+}
+
+function createRandomPositionWithinRoom() {
+	var x = createRandomCoordinateWithinRoom();
+	var y = createRandomCoordinateWithinRoom();
+	var z = createRandomCoordinateWithinRoom();
+	
+	return new THREE.Vector3( x, y, z );
+}
+
+function createRandomCoordinateWithinRoom() {
+	var maxDistanceFromCenter = ROOM_DIM / 2 - OBJECT_SIZE * 2;
+	return Math.random() * 2 * maxDistanceFromCenter - maxDistanceFromCenter;
+}
+
+function createLights() {
+	var point_light = new THREE.PointLight( 0xFFFFFF, 0.7);
+	point_light.position.set( 0, 0, 0 );
+	scene.add( point_light );
+	var camera_light = new THREE.PointLight( 0xFFFFFF, 0.4);
+	camera_light.position = camera.position;
+	scene.add( camera_light );
+}
+
+
+
+
+// ****** Animation loop function
+function animate() {
+	
+	requestAnimationFrame( animate );
+	render();
+	
+}
+
+
+
+// ****** Rendering function executed every refresh, responsible also for moving the camera
+function render() {
+	////////////////   UPDATE CAMERA POSITION   ///////////////
+	azimuth = calculateCameraAngleFromMouse( azimuth, mouse_x );
+	zenith = calculateCameraAngleFromMouse( zenith, mouse_y );
+	
+	decayCameraRotationalVelocity();
+	
+	camera.position.x = CAMERA_RADIUS * Math.sin( azimuth );
+	camera.position.y = CAMERA_RADIUS * -Math.sin( zenith );
+	camera.lookAt( CAMERA_TARGET );
+	
+	hibana.age();
+	
+	renderer.render( scene, camera );
+	
+}
+
+function calculateCameraAngleFromMouse( angle, mouse ) {
+	if ( mouse != 0 && angle != 0 )
+		return angle + mouse * MOUSE_SPEED *  ( 1 - ((mouse * angle) / Math.abs(mouse * angle))  * (Math.abs(angle)/ MAX_CAMERA_ANGLE) );
+	else
+		return angle + mouse * MOUSE_SPEED * ( 1 - (Math.abs(angle) / MAX_CAMERA_ANGLE) );
+}
+
+function decayCameraRotationalVelocity() {
+	if ( mouse_decay ) {
+		if ( Math.abs( mouse_x ) > 0.1 && Math.abs( mouse_y ) > 0.1 ) {
+			mouse_x /= 1.1;
+			mouse_y /= 1.1;
+		} else {
+			mouse_decay = false;
+			mouse_x = mouse_y = 0;
+		}
+	} else if ( !mouse_is_down ) {
+		var difference_x = CAMERA_HOME.x - camera.position.x;
+		var difference_y = CAMERA_HOME.y - camera.position.y;
+		mouse_x = Math.abs(difference_x) > 0.1 ? (difference_x / 10.0) * 6.0 : 0;
+		mouse_y = Math.abs(difference_y) > 0.1 ? -(difference_y / 10.0) * 6.0 : 0;
+	}
+}
---
+++ src/HIBANA.js
@@ -0,0 +1,170 @@
+// HIBANA.js
+// Particle engine for THREE.js
+// loosely based on Sparks.js
+// by Michael Barger
+
+HIBANA = function( scene, hidden_point ) {
+
+var PARTICLE_SIZE = 5.0;
+var DEFAULT_HIDDEN_COORD = -1000;
+var DEFAULT_HIDDEN_POINT = new THREE.Vector3( DEFAULT_HIDDEN_COORD, DEFAULT_HIDDEN_COORD, DEFAULT_HIDDEN_COORD );
+
+this.scene = scene;
+this.hidden_point = hidden_point || DEFAULT_HIDDEN_POINT;
+
+this.emitters = [];
+
+this.paused = true;
+
+this.material = __initialize_material( PARTICLE_SIZE );
+
+	function __initialize_material( particle_size ) {
+		var canvas = document.createElement( 'canvas' );
+		canvas.width = 16;
+		canvas.height = 16;
+
+		var context = canvas.getContext( '2d' );
+		var gradient = context.createRadialGradient( canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2 );
+		gradient.addColorStop( 0, 'rgba(255,255,255,.8)' );
+		gradient.addColorStop( 0.2, 'rgba(255,255,255,.6)' );
+		gradient.addColorStop( 0.4, 'rgba(255,255,255,.3)' );
+		gradient.addColorStop( 1, 'rgba(0,0,0,0)' );
+
+		context.fillStyle = gradient;
+		context.fillRect( 0, 0, canvas.width, canvas.height );
+		
+		var texture = new THREE.Texture( canvas );
+		texture.needsUpdate = true;
+		
+		return new THREE.ParticleBasicMaterial( { 	size: particle_size,
+													color: 0xEEEEEE,
+													map: texture,
+													blending: THREE.AdditiveBlending,
+													vertexColors: true,
+													transparent: true,
+													depthTest: false } );
+	}
+};
+
+
+HIBANA.prototype = {
+
+	constructor: HIBANA,
+	
+	addEmitter: function( parameters ) {
+	
+		parameters = parameters || {};
+		parameters.particle_count = parameters.particle_count || 200;
+		parameters.particle_color = parameters.particle_color || new THREE.Color( 0xFFFFFF );
+		parameters.rate = parameters.rate || 75;
+		parameters.acceleration = parameters.acceleration || new THREE.Vector3( 0, 0.25, 0 );
+		parameters.particle_life_expectancy_min = parameters.particle_life_expectancy_min || 5;
+		parameters.particle_life_expectancy_range = parameters.particle_life_expectancy_range || 30;
+		
+		var emitter = {};
+		emitter.geometry = new THREE.Geometry();
+		emitter.starting_position = THREE.GeometryUtils.randomPointsInGeometry( parameters.mesh.geometry, parameters.particle_count );
+		emitter.original_color = new THREE.Color().copy( parameters.particle_color );
+		
+		emitter.geometry.colors = [];
+		for ( var i = 0; i < parameters.particle_count; i++ ) {
+			emitter.geometry.vertices.push( new THREE.Vector3().copy( this.hidden_point ) );
+			emitter.geometry.colors.push( new THREE.Color().copy( emitter.original_color ) );
+		}
+		emitter.geometry.dynamic = true;
+		
+		emitter.system = new THREE.ParticleSystem( emitter.geometry, this.material );
+		emitter.system.position = parameters.mesh.position;
+		emitter.system.sortParticles = true;
+		/*
+		emitter.firstfew = new THREE.Color();
+		emitter.firstfew.setRGB( (1.0 - emitter.color.r) / FIRST_FEW_TICKS, (1.0 - emitter.color.g) / FIRST_FEW_TICKS, (1.0 - emitter.color.b) / FIRST_FEW_TICKS );
+		emitter.lastfew = new THREE.Color();
+		emitter.lastfew.setRGB( emitter.color.r / LAST_FEW_TICKS / 1.3, emitter.color.g / LAST_FEW_TICKS / 1.1, emitter.color.b / LAST_FEW_TICKS );
+		*/
+		emitter.active_particles = [];
+		emitter.next_particle = 0;
+		emitter.rate = parameters.rate;
+		emitter.acceleration = parameters.acceleration;
+		emitter.particle_life_expectancy_min = parameters.particle_life_expectancy_min;
+		emitter.particle_life_expectancy_range = parameters.particle_life_expectancy_range;
+		
+		this.scene.add( emitter.system );
+		this.emitters.push( emitter );
+		
+		return this;
+	},	
+	
+	pause: function() {
+		this.paused = true;
+	},
+	
+	
+	play: function() {
+		this.paused = false;
+	},
+	
+
+	age: function() {
+		if ( this.paused ) return this;
+		
+		__generateParticles = function( emitter ) {
+			var r = 100 * Math.random();
+			for ( var i = 0; i < Math.floor( r / (100.0 - emitter.rate)); i++ ) {
+				var new_particle = {};
+				
+				new_particle.vertex = emitter.geometry.vertices[ emitter.next_particle ];
+				new_particle.color = emitter.geometry.colors[ emitter.next_particle ];
+				new_particle.vertex.copy( emitter.starting_position[ emitter.next_particle ] );
+				new_particle.age = 0;
+				new_particle.life_expectancy = emitter.particle_life_expectancy_min + Math.random() * emitter.particle_life_expectancy_range;
+				new_particle.velocity = new THREE.Vector3().copy( emitter.acceleration );
+				
+				emitter.active_particles.push( new_particle );
+				
+				if ( ++emitter.next_particle >= emitter.geometry.vertices.length )
+					emitter.next_particle = 0;
+			}
+			
+			return this;
+		}
+
+		
+		for ( e in this.emitters ) {
+			__generateParticles( this.emitters[e] );
+			for ( p in this.emitters[e].active_particles ) {
+				if ( ++this.emitters[e].active_particles[p].age > this.emitters[e].active_particles[p].life_expectancy ) {
+					this.emitters[e].active_particles[p].vertex.copy( this.hidden_point );
+					this.emitters[e].active_particles[p].color.copy( this.emitters[e].original_color );
+					this.emitters[e].active_particles.splice( p, 1 );
+				} else {
+					// position/velocity change
+					/*
+					emitter.active_particles[p].velocity.x += PARTICLE_DRIFT_OFFSET - PARTICLE_DRIFT * Math.random();
+					emitter.active_particles[p].velocity.y -= PARTICLE_GRAVITY;
+					emitter.active_particles[p].velocity.z += PARTICLE_DRIFT_OFFSET - PARTICLE_DRIFT * Math.random();
+					*/
+					this.emitters[e].active_particles[p].vertex.addSelf( this.emitters[e].active_particles[p].velocity );
+					
+					// color change
+					/*
+					var timeRemaining = emitter.particles[p].lifeExpectancy - emitter.particles[p].age;
+
+					if ( emitter.active_particles[p].age <= FIRST_FEW_TICKS ) {
+						emitter.active_particles[p].color.r -= emitter.firstfew.r;
+						emitter.active_particles[p].color.g -= emitter.firstfew.g;
+						emitter.active_particles[p].color.b -= emitter.firstfew.b;
+					} else if ( timeRemaining <= LAST_FEW_TICKS ) {
+						emitter.active_particles[p].color.r -= emitter.lastfew.r;
+						emitter.active_particles[p].color.g -= emitter.lastfew.g;
+						emitter.active_particles[p].color.b -= emitter.lastfew.b;
+					}
+					*/
+				}
+			}
+			this.emitters[e].geometry.verticesNeedUpdate = true;
+			this.emitters[e].geometry.colorsNeedUpdate = false;
+		}	
+		return this;
+	}
+}
